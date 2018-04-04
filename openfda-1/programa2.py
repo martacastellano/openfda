95deef4eae12198e8061daea68b0995496c1413c
martacastellano <martacastellano99@gmail.com>
2018-04-04 23:35:23 +0200

    openfda1

---
+++ openfda-1/programa2.py
@@ -0,0 +1,24 @@
+#partimos del mismo código del programa1
+import http.client
+import json
+
+
+headers = {'User-Agent': 'http-client'}
+
+
+conn = http.client.HTTPSConnection("api.fda.gov")
+conn.request("GET", "/drug/label.json?limit=10", None, headers) #como buscamos información de
+                                                                #10 medicamentos ponemos limit=10
+
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+
+repos_raw = r1.read().decode("utf-8")
+conn.close()
+
+repos = json.loads(repos_raw)
+
+#itero con un bucle for para acceder a la información de los 10 medicamentos
+for medicamento in range(10):
+    datos= repos['results'][medicamento]
+    print('El medicamento', medicamento+1,' tiene como id: ',datos['id'])
